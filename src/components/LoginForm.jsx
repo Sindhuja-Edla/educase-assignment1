83d37dc49e59d448aeb6dab1cf1859c69a32b1c8
Sindhujaedla <sindhujaedla9@gmail.com>
2025-06-17 22:54:53 +0530

    first commit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -10,7 +10,7 @@ const LoginForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:5000/api/auth/login', {
+      const res = await fetch('https://educase-assignment-becw.onrender.com//api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
